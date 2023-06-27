e00d19bdf6dd5c8ea8ef358c07fe84fb78d89631
Sharana Sabesan <63738475+sharana-sabesan@users.noreply.github.com>
2023-06-27 13:53:33 -0700

    Update and rename r.sol to Conditionals_And_Loops.sol

---
+++ Solidity_Tut_1/Conditionals_And_Loops.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Conditionals_And_Loops {
+    // Conditionals  = if/else 
+    // Loops = for & while
+    uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function countEvenNumbers() public view returns(uint) {
+        uint count = 0; 
+
+        for(uint i = 0;  i < numbers.length; i++) {
+            if(isEvenNumber(numbers[i])) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    function isEvenNumber(uint _number) public view returns(bool) {
+        if(_number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isOwner() public view returns(bool) {
+        return(msg.sender == owner);
+    }
+
+
+}
